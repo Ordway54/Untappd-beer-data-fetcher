f2c2ac1c7dc84e5703d8ce26f1cbfbf76fe31b54
Ordway54 <47861779+Ordway54@users.noreply.github.com>
2022-04-19 17:03:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,26 @@ client_id = '' # redacted
 client_secret = '' # redacted
 access_token = '' # redacted
 
-csv_header = ["Date First Had","Beer Name","Beer Style","Brewery Name","ABV","IBU","User Rating","Global Rating","Brewery City","Brewery State","Country","Brewery Type"]
+csv_header = ["Unique Number","Date First Had","Beer Name","Beer Style","Brewery Name","ABV","IBU","User Rating","Global Rating","Brewery City","Brewery State","Country","Brewery Type"]
 csv_data = [] # This will be a list of lists. Each nested list will be one row of the CSV, or, one beer's data.
 num_checkins_recorded = 0 # counter variable for determining offset value of each API call
 total_user_distinct_count = 0
 
 def make_API_request(first_API_call:bool):
+    """Makes a call to Untappd's API. Returns data on 50 distinct user beers."""
     global num_checkins_recorded, total_user_distinct_count
 
-    if first_API_call is True:
-        response = requests.get(f"https://api.untappd.com/v4/user/beers/?access_token={access_token}&limit=50").json()
-    else:
-        response = requests.get(f"https://api.untappd.com/v4/user/beers/?access_token={access_token}&offset={num_checkins_recorded}&limit=50").json()
+    if first_API_call is True: # on first API call, no offset parameter is needed in the API endpoint URL
+        response = requests.get(f"https://api.untappd.com/v4/user/beers?access_token={access_token}&limit=50").json()
+    else: # offset parameter included in API endpoint URL
+        response = requests.get(f"https://api.untappd.com/v4/user/beers?access_token={access_token}&offset={num_checkins_recorded}&limit=50").json()
     
     distinct_beers = response['response']['beers']['items'] # this is a list of dictionaries
     total_user_distinct_count = response['response'].get('total_count')
 
     for beer in distinct_beers: # beer is a dictionary
         beer_data = [] # create an empty list and append the following data to it
+        beer_data.append(total_user_distinct_count - num_checkins_recorded) # calculates unique number in descending order
         beer_data.append(beer.get('first_had')) # date beer was first checked in
         beer_data.append(beer.get('beer').get('beer_name'))
         beer_data.append(beer.get('beer').get('beer_style'))
@@ -39,16 +41,19 @@ def make_API_request(first_API_call:bool):
         print(f'Distinct beers recorded: {num_checkins_recorded} / {total_user_distinct_count}')
         csv_data.append(beer_data) # append list of data for each beer to master list
 
-make_API_request(True)
+make_API_request(True) # first API call, True is passed to denote it's the first call
 
 while num_checkins_recorded < total_user_distinct_count:
     make_API_request(False)
 
-with open('Untappd_distinctbeers_JO_041222.csv','w',newline='',encoding='utf-8') as f:
+print("Data collection completed successfully.")
+print("Writing data to CSV file...")
+
+with open('Untappd_distinct_beers.csv','w',newline='',encoding='utf-8') as f:
     csv_writer = csv.writer(f)
     csv_writer.writerow(csv_header)
 
     for line in csv_data:
         csv_writer.writerow(line)
 
-print("Data collection complete.")
+print("Data written to CSV successfully.\n\nProcess complete.")
